3cfc8dac3f6e897d79fd34aec61b8ba7841ea210
OdelDevelopersTest1 <odeldeveloperstest1@gmail.com>
2024-11-27 13:05:24 -0800

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+    moduleNameMapper: {
+      '^@/components/(.*)$': '<rootDir>/src/components/$1',
+      '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
+    },
+  };
